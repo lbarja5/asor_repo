b7a706562057acbf0cb7019cd6d7a7876faece10
Lidia <root@localhost.localdomain>
2020-12-05 15:31:50 +0100

    ej6

---
+++ practica2.1/ejercicio6.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	long argmax = sysconf(_SC_ARG_MAX);
+        long maxchild = sysconf(_SC_CHILD_MAX);
+        long maxopen = sysconf(_SC_OPEN_MAX);	
+	if(argmax == -1 && maxchild == -1 && maxopen == -1)
+		perror("Se ha producido un error en sysconf");
+	else
+	{
+		printf("Longitud máxima de argumentos: %ld\n", argmax);
+		printf("Número máximo de hijos: %ld\n", maxchild);
+		printf("Número máximo de ficheros abiertos: %ld\n", maxopen);
+	}	
+	return 0;
+}
+
