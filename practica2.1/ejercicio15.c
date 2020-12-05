bdff04dbccee1a2faf5c499022ea05f6064b3e31
Lidia <lbarja@ucm.es>
2020-12-05 20:16:08 +0100

    15 variables

--- practica2.1/ejercicio15.c
+++ practica2.1/ejercicio15.c
@@ -11,21 +11,24 @@ int main()
 	struct tm *tiempo;
 	time_t t;
 	time(&t);
-	char s[200];
+	char semana[20], dia[10],mes[10],hora[10],min[10];
 	tiempo = localtime(&t);
 	setlocale(LC_ALL,"es_ES");
 	if(tiempo == NULL) {
 		perror("Se ha producido un error en localtime");
 		return -1;
 	}
-	char format[10] = "%A";
-	if(strftime(s,sizeof(s),format,tiempo) == 0)
+	if(strftime(semana,sizeof(semana),"%A",tiempo) == 0||
+		strftime(dia,sizeof(dia),"%d",tiempo) == 0||
+		strftime(mes,sizeof(mes),"%B",tiempo) == 0||
+		strftime(hora,sizeof(hora),"%H",tiempo) == 0||
+		strftime(min,sizeof(min),"%M",tiempo) == 0)
 	{
 		printf("Error en strftime");
 		return -1;
 		
 	}
-	printf("%s",s);
+	printf("%s, %s de %s de %i, %s:%s",semana,dia, mes,tiempo->tm_year+1900,hora,min);
 	return 0;
 }
 
