be75d1b2628f6f83addde284f4a6bd3a314e766e
Lidia <lbarja@ucm.es>
2020-12-05 18:01:22 +0100

    12

---
+++ practica2.1/ejercicio12.c
@@ -0,0 +1,11 @@
+//ejercicio 12
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(){
+	printf("Hora: %ld segundos\n",time(0));
+	return 0;
+}
