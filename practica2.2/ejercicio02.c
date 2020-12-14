9923bc8adff6ef99a20b6f43eb2396571de5510a
Lidia <lbarja@ucm.es>
2020-12-14 11:27:20 +0100

    ej2

---
+++ practica2.2/ejercicio02.c
@@ -0,0 +1,6 @@
+//# ls -ld
+//drwxr-xr-x 2 root root 53 Dec 14 11:15 .
+//tipo: d directorio
+//rwx_propietario: todos los permisos: 7
+//rwx_grupo: lectura y ejecución: 5
+//rwx_resto: lectura y ejecución: 5
