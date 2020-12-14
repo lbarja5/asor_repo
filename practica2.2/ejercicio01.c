de999182ac5f7c3f563f8ca56e33c6210eff75c8
Lidia <lbarja@ucm.es>
2020-12-14 11:31:34 +0100

    mejora vista

--- practica2.2/ejercicio01.c
+++ practica2.2/ejercicio01.c
@@ -1,23 +1,23 @@
 //# ls
 //ejercicio01.c
-//
+//---------------------------------------------------------------
 //	-a:	no ignora las entradas que empiezan en .
 //.  ..  ejercicio01.c  .ejercicio01.c.swp
-//
+//--------------------------------------------------------------
 //	-l:	usa un formato de lista detallada
 //total 4
 //-rw-r--r-- 1 root root 5 Dec 14 10:46 ejercicio01.c
-//
+//--------------------------------------------------------------
 //	-d:	lista de directorios, no su contenido
 //.
-//
+//--------------------------------------------------------------
 //	-h:	con -l y -s, imprime tamaños como 1K 234M 2G etc.
 //total 4.0K
 //4.0K -rw-r--r-- 1 root root 5 Dec 14 10:46 ejercicio01.c
-//
+//--------------------------------------------------------------
 //	-i:	imprime el numero index de cada fichero
 //17153643 ejercicio01.c
-//
+//--------------------------------------------------------------
 //	-R:	lista subdirectorios recursivamente
 //:
 //practica2.1  practica2.2
@@ -29,13 +29,13 @@
 //
 //./practica2.2:
 //ejercicio01.c
-//
+//--------------------------------------------------------------
 //	-1:	lista un fichero por linea
 //practica2.1
 //practica2.2
-//
+//--------------------------------------------------------------
 //	-F:	añade indicadores o simbolos a las entradas
 //practica2.1/  practica2.2/
-//
+//--------------------------------------------------------------
 //	--color: colorea la salida
 //
