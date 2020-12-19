5bebd66563ab514311c2e6a7d37f36726c61bde1
Lidia <lbarja@ucm.es>
2020-12-19 15:41:15 +0100

    correccion: ";"

--- practica2.2/ejercicio07.c
+++ practica2.2/ejercicio07.c
@@ -7,7 +7,7 @@
 int main()
 {
 	if(remove("5.txt") == 0){
-		mode_t mascara = umask(0027)
+		mode_t mascara = umask(0027);
 		int fd = open("5.txt", O_RDWR|O_CREAT, mascara);
 		if(fd == -1)
 			printf("ERROR en open");
@@ -15,7 +15,7 @@ int main()
 			printf("Se ha creado el fichero");
 		close(fd);
 	}else{
-		perror("El fichero ya existe y no sido posible borrarlo")
+		perror("El fichero ya existe y no sido posible borrarlo");
 	}
 	return 0;
 }
