6dc08bb9d2114e410f5fc36cf7d56dec18a21408
Lidia <lbarja@ucm.es>
2020-12-19 21:36:49 +0100

    12 fin 12 fin 12 fin 12 fin 12 fin 12 fin 12 fin 12 fin 12 fin 12 fin 12 fin 12 fin

--- practica2.2/ejercicio12.c
+++ practica2.2/ejercicio12.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 //Redirigir la salida estandar a un fichero argv[1]
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-        	fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+	if (argc < 3) {
+        	fprintf(stderr, "Usage: %s <file> <text>\n", argv[0]);
         	return -1;
         }
 	
@@ -15,6 +18,10 @@ int main(int argc, char **argv)
 	
 	dup2(fd, 1);
 	close(fd);
-	printf("%s\n",argv[1]);
+	int i = 2;
+	while(i < argc){
+		printf("%s\n",argv[i]);
+		i++;
+	}
 	return 0;
 }
