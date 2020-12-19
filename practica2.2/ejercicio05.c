9fe2b93b734553b3062d28f260c7e95ec2883237
Lidia <lbarja@ucm.es>
2020-12-19 13:39:37 +0100

    ej5

---
+++ practica2.2/ejercicio05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd = open("5.txt", O_RDWR|O_CREAT, 0645);
+	if(fd == -1)
+		printf("ERROR en open");
+	return 0;
+}
+//# ls -l
+//-rw-r--r-x 1 root root    0 Dec 19 13:37 5.txt
