748c15e5318f71b4da7bf4e3789eff010f0daa88
Lidia <lbarja@ucm.es>
2020-12-19 22:41:18 +0100

    ej13

---
+++ practica2.2/ejercicio13.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+//Redirigir la salida estandar a un fichero argv[1]
+int main(int argc, char **argv)
+{
+	if (argc < 3) {
+        	fprintf(stderr, "Usage: %s <file> <text>\n", argv[0]);
+        	return -1;
+        }
+	
+	int fd;
+	
+	fd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0644);
+	
+	dup2(fd, 1);
+	dup2(fd, 2);
+	close(fd);
+	
+	int i = 2;
+	while(i < argc){
+		printf("%s\n",argv[i]);
+		i++;
+	}
+	//Generar un erro
+	perror("Esto es un error");
+	
+	return 0;
+}
