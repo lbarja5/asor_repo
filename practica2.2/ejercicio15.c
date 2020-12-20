91f26015a7dd5ac43444996aad3c8b7fcdfd4dc7
Lidia <lbarja@ucm.es>
2020-12-20 01:31:09 +0100

    libs 15

--- practica2.2/ejercicio15.c
+++ practica2.2/ejercicio15.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
 
 int main(int argc, char **argv)
 {
@@ -11,14 +14,14 @@ int main(int argc, char **argv)
         }
 	//Mostrar el estado de un cerrojo sobre un fichero
 	int fd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0644);
-	errono = lockf(fd, F_TLOCK, 0);
-	if(errono != EAGAIN)
+	errno = lockf(fd, F_TLOCK, 0);
+	if(errno != EAGAIN)
 	{
 		//Desbloqueado -> Fijar el cerrojo y escribir la hora actual.
 		//Sleep 30seg y liberarlo
 		printf("Desbloqueado\n");
 		char hora[10];
-		strftime(hora,sizeof(hora),"%R",time(0))
+		strftime(hora,sizeof(hora),"%R",time(0));
 		printf("Hora: %s\n",hora);
 		sleep(30);
 		lockf(fd, F_ULOCK, 0);
