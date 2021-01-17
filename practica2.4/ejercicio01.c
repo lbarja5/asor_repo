67f69dcf565b2f6cf4447ca05286efbbc9139771
Lidia <lbarja@ucm.es>
2021-01-17 17:26:55 +0100

    inicio 1

---
+++ practica2.4/ejercicio01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+
+    //Crea tuberia
+    int fd[2];
+    pipe(fd);
+    //fork
+    pid_t pid = fork();
+
+    switch(pid) {
+        case -1:
+            perror("fork");
+            exit(1);
+        case 0: //Hijo
+            dup2(fd[1], 1);
+            close(fd[1]);
+            close(fd[0]);
+            execlp(argv[1], argv[1], argv[2], NULL);
+            break;
+        default: //Padre
+            dup2(fd[0], 0);
+
+            close(fd[0]);
+            close(fd[1]);
+
+            execlp(argv[3], argv[3], argv[4], NULL);
+            break;
+    }
+    return 0;
+}   
