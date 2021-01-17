5f0c2e5520f66918980c16cd1d3a239b284bfa72
Lidia <lbarja@ucm.es>
2021-01-17 20:10:10 +0100

    ej3

---
+++ practica2.4/ejercicio03.c
@@ -0,0 +1,13 @@
+$ mkfifo tub
+$ ls -l tub
+prw-rw-r-- 1 cursoredes cursoredes 0 Jan 17 20:00 tub
+$ stat tub
+  File: ‘tub’
+  Size: 0         	Blocks: 0          IO Block: 4096   fifo
+Device: fd00h/64768d	Inode: 3350932     Links: 1
+Access: (0664/prw-rw-r--)  Uid: ( 1000/cursoredes)   Gid: ( 1000/cursoredes)
+Access: 2021-01-17 20:00:53.172107579 +0100
+Modify: 2021-01-17 20:00:53.172107579 +0100
+Change: 2021-01-17 20:00:53.172107579 +0100
+ Birth: -
+
