8d2171a76f60de2192b267b5f209a5b7bc17f25e
Lidia <lbarja@ucm.es>
2021-01-17 15:53:37 +0100

    ej 11

--- practica2.3/ejercicio11.c
+++ practica2.3/ejercicio11.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
 
 //Bloquear SIGINT y SIGTSTP
 int main(){
-    if(setenv("SLEEP_SECS","5",0) != 0){
+    if(setenv("SLEEP_SECS","20",0) != 0){
         perror("setenv");
         return -1;
     }
-    sigset_t blk_set;
+    sigset_t blk_set, pend;
     sigemptyset(&blk_set);
     sigaddset(&blk_set, SIGINT);
     sigaddset(&blk_set, SIGTSTP);
@@ -16,9 +17,17 @@ int main(){
     char* tiempo = getenv("SLEEP_SECS");
     int t = atoi(tiempo);
     sleep(t);
-    if(sigpending(&blk_set) != 0){
+
+    if(sigpending(&pend) != 0){
         perror("sigpending");
         return -1;
     }
-    sigprocmask(SIG_UNBLOCK, &blk_set, NULL);
+    if(sigismember(&pend,SIGINT)){
+        printf("Se ha detectado la señal SIGINT\n");
+    }
+    if(sigismember(&pend,SIGTSTP)){
+        printf("Se ha detectado la señal SIGTSTP. El programa se detendrá\n");
+        sigprocmask(SIG_UNBLOCK, &pend, NULL);
 }
+    }
+    
