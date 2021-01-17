de59ba979a25f22a374f3254d5aa047b1a7b0f96
Lidia <lbarja@ucm.es>
2021-01-17 17:58:44 +0100

    ej1

--- practica2.4/ejercicio01.c
+++ practica2.4/ejercicio01.c
@@ -7,6 +7,11 @@
 
 int main(int argc, char **argv){
 
+     if (argc < 5) {
+        fprintf(stderr, "Usage: %s <command 1> <command 2>\n", argv[0]);
+        return -1;
+    }
+
     //Crea tuberia
     int fd[2];
     pipe(fd);
@@ -17,19 +22,21 @@ int main(int argc, char **argv){
         case -1:
             perror("fork");
             exit(1);
-        case 0: //Hijo
-            dup2(fd[1], 1);
-            close(fd[1]);
-            close(fd[0]);
-            execlp(argv[1], argv[1], argv[2], NULL);
-            break;
-        default: //Padre
+        case 0: //Hijo lee de la tuberia
             dup2(fd[0], 0);
 
+            close(fd[1]);
+            close(fd[0]);
+
+            execlp(argv[3], argv[3], argv[4], NULL);
+            break;
+        default: //Padre escibe en la tuberia
+            dup2(fd[1], 1);
+
             close(fd[0]);
             close(fd[1]);
 
-            execlp(argv[3], argv[3], argv[4], NULL);
+            execlp(argv[1], argv[1], argv[2], NULL);
             break;
     }
     return 0;
