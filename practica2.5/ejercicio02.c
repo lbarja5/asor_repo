d2662e8693cbc0a83012a7e8619e3f3bc2159234
Lidia <lbarja@ucm.es>
2021-01-24 17:17:24 +0100

    ej2ej2

--- practica2.5/ejercicio02.c
+++ practica2.5/ejercicio02.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <time.h>
 
 int main(int argc, char** argv){
     if (argc != 3) {
@@ -30,7 +31,7 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    int sd = socket(res->ai_family, res->ai_socktype,0);
+    int sd = socket(res->ai_family, res->ai_socktype, 0);
 
     bind(sd, res->ai_addr, res->ai_addrlen);
 
@@ -44,13 +45,32 @@ int main(int argc, char** argv){
         socklen_t client_len = sizeof(client_addr);
 
         int bytes = recvfrom(sd, buffer, 79, 0, (struct sockaddr *) &client_addr, &client_len);
-
+        printf("B: %s\n", &buffer);
+        char msg = buffer[0];
         buffer[bytes]='\0';
 
         getnameinfo((struct sockaddr *) &client_addr, client_len, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-
-        printf("%i bytes de %s:%s\n",bytes, host, serv);
+        
+        size_t tam;
+        switch(msg){
+            case 't':
+                printf("%i bytes de %s:%s\n", bytes, host, serv);
+                tam = strftime(buffer, sizeof(buffer), "%r", localtime(0));
+                sendto(sd, buffer, tam,0,(struct sockaddr *) &client_addr, client_len);
+                break;
+            case 'd':
+                printf("%i bytes de %s:%s\n", bytes, host, serv);
+                tam = strftime(buffer, sizeof(buffer), "%D", localtime(0));
+                sendto(sd, buffer, tam,0,(struct sockaddr *) &client_addr, client_len);
+                break;
+            default:
+                printf("Comando no soportado %s", buffer);
+                break;
+        }
     }
+
+    //Cerrar socket
+    close(sd);
 
     return 0;
 }
